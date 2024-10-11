6f34e80e7572546e88eccab9137eae57723765aa
julienrull <julien.rull38@gmail.com>
2024-10-11 10:50:15 +0200

    [IMP] DWM start script 'startdwm.sh' for 'hot reloading'

---
+++ startdwm.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+xrdb ~/.Xresources
+setxkbmap -model pc105 -layout fr -option altwin:swap_lalt_lwin
+picom -b --no-fading-openclose
+xset r rate 200 25
+xrandr --output DP-1 --primary --mode 3840x2160 --output eDP-1 --off
+feh --bg-fill ~/Git/dotfiles/wp.png
+dwmblocks &
+pipewire &
+
+while true; do
+    dbus-run-session dwm 2> ~/.dwm.log
+done
+
